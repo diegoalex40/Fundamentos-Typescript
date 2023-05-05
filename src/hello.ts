a486d043e9ca7c199d8ec4e04b5afa4413b2d003
Diego Yanez <diego_alex40@hotmail.com>
2023-05-05 09:56:44 -0500

    Configuracion basica del proyecto en TypeScript

---
+++ src/hello.ts
@@ -0,0 +1,10 @@
+var saludo = "Hola mundo";
+console.log(saludo);
+
+let contador = 0;
+for (let index = 0; index <= 10; index++)
+    {
+      contador = index;
+      console.log(contador);
+      contador = 0;
+    };
